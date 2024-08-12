ea0f4f91cf8b071c2938ade124b912c8d4cd433e
Quang Bui <buiminhquang188@gmail.com>
2024-08-12 09:12:27 +0700

    Initial Project

---
+++ src/main/java/org/cybersoft/bookingticketcinemabe/BookingTicketCinemaBeApplication.java
@@ -0,0 +1,13 @@
+package org.cybersoft.bookingticketcinemabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTicketCinemaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingTicketCinemaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cybersoft/bookingticketcinemabe/BookingTicketCinemaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cybersoft.bookingticketcinemabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTicketCinemaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
