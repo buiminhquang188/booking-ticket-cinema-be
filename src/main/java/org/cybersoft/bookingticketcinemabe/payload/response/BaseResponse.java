9112c6dfd502f240e2d7c1e926fcf943c1a5dc5f
Hung Tran <tran.huy.hung.20.01@gmail.com>
2024-08-31 23:09:54 +0700

    adjust getAllUsers as required

--- src/main/java/org/cybersoft/bookingticketcinemabe/payload/response/BaseResponse.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/payload/response/BaseResponse.java
@@ -1,10 +1,12 @@
 package org.cybersoft.bookingticketcinemabe.payload.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
 public class BaseResponse<T> {
     private int statusCode = 200;
     private  String message;
