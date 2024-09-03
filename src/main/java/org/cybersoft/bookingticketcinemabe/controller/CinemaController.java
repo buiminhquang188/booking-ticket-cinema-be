d15a5059104a84c0ce158692952da7f2ab80700e
Quang Bui <buiminhquang188@gmail.com>
2024-09-03 16:24:56 +0700

    Feature: Implement Get Cinema

--- src/main/java/org/cybersoft/bookingticketcinemabe/controller/CinemaController.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/controller/CinemaController.java
@@ -2,6 +2,7 @@ package org.cybersoft.bookingticketcinemabe.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,4 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface CinemaController {
     @GetMapping("/cinemas")
     ResponseEntity<?> getCinemas(@RequestParam int pageNo, @RequestParam int pageSize, @RequestParam(required = false) String name);
+
+    @GetMapping("/cinema/{id}")
+    ResponseEntity<?> getCinema(@PathVariable int id);
 }
