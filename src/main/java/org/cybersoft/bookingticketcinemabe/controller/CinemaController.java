3545319448c95324955f50e59205f6c5d83c0019
Quang Bui <buiminhquang188@gmail.com>
2024-09-29 12:40:14 +0700

    Feature: Implement Update Cinema

--- src/main/java/org/cybersoft/bookingticketcinemabe/controller/CinemaController.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/controller/CinemaController.java
@@ -1,6 +1,7 @@
 package org.cybersoft.bookingticketcinemabe.controller;
 
 import org.cybersoft.bookingticketcinemabe.payload.request.CinemaCreationRequest;
+import org.cybersoft.bookingticketcinemabe.payload.request.CinemaUpdateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,4 +15,7 @@ public interface CinemaController {
 
     @PostMapping("/cinema")
     ResponseEntity<?> createCinema(@RequestBody CinemaCreationRequest cinemaCreationRequest);
+
+    @PatchMapping("/cinema/{id}")
+    ResponseEntity<?> updateCinema(@PathVariable Integer id, @RequestBody CinemaUpdateRequest cinemaUpdateRequest);
 }
--- src/main/java/org/cybersoft/bookingticketcinemabe/repository/CinemaProvinceRepository.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/repository/CinemaProvinceRepository.java
@@ -5,6 +5,11 @@ import org.cybersoft.bookingticketcinemabe.entity.key.IdCinemaProvince;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CinemaProvinceRepository extends JpaRepository<CinemaProvinceEntity, IdCinemaProvince> {
+    List<CinemaProvinceEntity> findAllByCinemaId(Integer cinemaId);
+
+    CinemaProvinceEntity findByCinemaIdAndProvinceId(Integer cinemaId, Integer provinceId);
 }
