027bab78bd8ef67dda8f5c0a7fe4afcef85d99c5
Quang Bui <buiminhquang188@gmail.com>
2024-10-17 19:52:04 +0700

    [CBTC-27] Fix SQL fk

--- src/main/resources/sql/hall.sql
+++ src/main/resources/sql/hall.sql
@@ -1,5 +1,5 @@
 SET NAMES utf8mb4;
 
-INSERT INTO capstone.hall (hall_id, id, total_seats, created_at, updated_at, name)
+INSERT INTO capstone.hall (branch_id, id, total_seats, created_at, updated_at, name)
 VALUES (1, 1, 0, '2024-10-14 20:24:50.000000', '2024-10-15 20:39:38.277663', 'A
 ');
