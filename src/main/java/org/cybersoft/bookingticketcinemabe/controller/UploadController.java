3df7428ac697e9e586afb0d1dffcb3fd96f9862f
Hung Tran <tran.huy.hung.20.01@gmail.com>
2024-10-25 00:11:22 +0700

    adjusted endpoint upload image

--- src/main/java/org/cybersoft/bookingticketcinemabe/controller/UploadController.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/controller/UploadController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@RequestMapping("/api/image")
+@RequestMapping
 public interface UploadController {
 
     @PostMapping("/upload")
