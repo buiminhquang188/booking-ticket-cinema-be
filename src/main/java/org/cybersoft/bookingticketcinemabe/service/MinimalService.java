72454b4ce8d07bfb01442a0e046cb7baffcd3f5c
Quang Bui <buiminhquang188@gmail.com>
2024-10-26 21:22:07 +0700

    [CBTC-101] Implement Get Minimal List for Cinema & District, able to filter by name

--- src/main/java/org/cybersoft/bookingticketcinemabe/service/MinimalService.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/service/MinimalService.java
@@ -1,6 +1,10 @@
 package org.cybersoft.bookingticketcinemabe.service;
 
+import org.cybersoft.bookingticketcinemabe.dto.MinimalDTO;
 import org.cybersoft.bookingticketcinemabe.dto.PageableDTO;
+import org.cybersoft.bookingticketcinemabe.payload.request.MinimalCriteria;
+
+import java.util.List;
 
 public interface MinimalService {
     PageableDTO<?> getScreenings(int pageNo, int pageLimit, String sortBy);
@@ -8,4 +12,8 @@ public interface MinimalService {
     PageableDTO<?> getMovies(int pageNo, int pageLimit, String sortBy);
 
     PageableDTO<?> getBranches(int pageNo, int pageLimit, String sortBy);
+
+    PageableDTO<List<MinimalDTO>> getCinemas(MinimalCriteria minimalCriteria);
+
+    PageableDTO<List<MinimalDTO>> getDistricts(MinimalCriteria minimalCriteria);
 }
