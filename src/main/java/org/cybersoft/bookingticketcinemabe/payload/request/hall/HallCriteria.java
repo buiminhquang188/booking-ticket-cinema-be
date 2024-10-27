0978fbfb33295f85ca888efc20ea6e8aa1ce6a84
Quang Bui <buiminhquang188@gmail.com>
2024-10-27 19:38:33 +0700

    [CBTC-103] Update ID for Hall Criteria

--- src/main/java/org/cybersoft/bookingticketcinemabe/payload/request/hall/HallCriteria.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/payload/request/hall/HallCriteria.java
@@ -7,6 +7,7 @@ import org.cybersoft.bookingticketcinemabe.payload.request.pagination.Pagination
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class HallCriteria extends Pagination {
+    private Integer id;
     private String name;
     private Integer totalSeats;
     private String branchId;
