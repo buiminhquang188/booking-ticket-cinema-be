2de69a65de437679bcff1d24db42044c3d85f0ff
Quang Bui <buiminhquang188@gmail.com>
2024-11-05 21:05:59 +0700

    [CBTC-99] Remove unnecessary screening response dto

--- src/main/java/org/cybersoft/bookingticketcinemabe/dto/screening/ScreeningDTO.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/dto/screening/ScreeningDTO.java
@@ -4,21 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.cybersoft.bookingticketcinemabe.dto.ReservationDTO;
-import org.cybersoft.bookingticketcinemabe.dto.ScreeningSeatDTO;
-import org.cybersoft.bookingticketcinemabe.dto.SeatReservationDTO;
 import org.cybersoft.bookingticketcinemabe.dto.hall.HallDetailDTO;
 import org.cybersoft.bookingticketcinemabe.dto.movie.MovieDetailDTO;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ScreeningDTO {
-
     private Integer id;
 
     private LocalDateTime startTime;
@@ -28,11 +23,4 @@ public class ScreeningDTO {
     private HallDetailDTO hall;
 
     private MovieDetailDTO movie;
-
-    private List<ScreeningSeatDTO> screeningSeats;
-
-    private List<ReservationDTO> reservations;
-
-    private List<SeatReservationDTO> seatReservations;
-
 }
