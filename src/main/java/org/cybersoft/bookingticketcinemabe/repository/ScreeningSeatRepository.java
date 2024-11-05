5fe777479d538c988234b0c00868d313b7c05b5e
Quang Bui <buiminhquang188@gmail.com>
2024-11-05 20:39:12 +0700

    [CBTC-99] Fix: Validation reservation time & Get seat with screeningId

--- src/main/java/org/cybersoft/bookingticketcinemabe/repository/ScreeningSeatRepository.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/repository/ScreeningSeatRepository.java
@@ -4,6 +4,9 @@ import org.cybersoft.bookingticketcinemabe.entity.ScreeningSeatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScreeningSeatRepository extends JpaRepository<ScreeningSeatEntity, Integer> {
+    List<ScreeningSeatEntity> findAllByIdInAndScreeningId(List<Integer> seatIds, Integer screeningId);
 }
