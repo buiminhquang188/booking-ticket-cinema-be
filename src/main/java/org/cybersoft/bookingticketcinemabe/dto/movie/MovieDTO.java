1a4ccc7bd74bb93f68bbedeb95ec1ce3bd3c23a0
Hung Tran <tran.huy.hung.20.01@gmail.com>
2024-11-06 22:24:07 +0700

    Fix format localDateTime response

--- src/main/java/org/cybersoft/bookingticketcinemabe/dto/movie/MovieDTO.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/dto/movie/MovieDTO.java
@@ -1,5 +1,6 @@
 package org.cybersoft.bookingticketcinemabe.dto.movie;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +22,7 @@ public class MovieDTO {
 
     private Integer time;  // Time in minutes or another appropriate unit
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
 
     private String name;
--- src/main/java/org/cybersoft/bookingticketcinemabe/dto/screening/ScreeningDTO.java
+++ src/main/java/org/cybersoft/bookingticketcinemabe/dto/screening/ScreeningDTO.java
@@ -1,5 +1,6 @@
 package org.cybersoft.bookingticketcinemabe.dto.screening;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +16,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ScreeningDTO {
     private Integer id;
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startTime;
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endTime;
-
     private HallDetailDTO hall;
-
     private MovieDetailDTO movie;
 }
